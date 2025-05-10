f1f904b5367231be62858c4623f8b87d3812d4ac
Firdavs Yuldashev <firdavsyuldashev111@gmail.com>
2025-05-10 22:47:55 +0500

    van-list page has been added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,9 +8,8 @@ const Navbar = () => {
   return (
     <header>
     <nav className='nav'>
-        <img src={logo} alt="#Vanlife logo"/>
+        <Link to={"/"}><img src={logo} alt="#Vanlife logo"/></Link>
         <ul className='nav-list'>
-            <li className='nav-item'><Link to={"/"}>Home</Link></li>
             <li className='nav-item'><Link to={"/about"}>About</Link></li>
             <li className='nav-item'><Link to={"/vans"}>Vans</Link></li>
         </ul>
--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
 import "./Vans.css"
 
 const Vans = () => {
+
+const [vans, setVans] = React.useState(null); 
+console.log(vans)
+
+React.useEffect(() => {
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch("/api/vans");
+            if (!response.ok) throw new Error("Network response was not ok");  
+            const data = await response.json()
+            setVans(data.vans);  
+        } catch(err) {
+            console.error("Fetch error:", err); 
+        }
+    }
+
+    fetchData(); 
+    
+}, []) 
+
+
   return (
-    <h1>Vans page goes here 🚐</h1>
+    <>
+    <Navbar/>
+    <main className='vans-page'>
+    <h1>Explore our van options</h1>
+    <div className="filters-container">
+        <button className='btn simple-type'>Simple</button>
+        <button className='btn luxury-type'>Luxury</button>
+        <button className='btn rugged-type'>Rugged</button>
+        <button className='clear-btn'>Clear filters</button>
+    </div>
+    <section className='vans-list'>
+        {vans &&
+            vans.map(van => (
+                <div key={van.id} className="van">
+                    <img src={van.imageUrl} alt={`Van ${van.name}`} className='van-img'/>
+                    <h2 className='van-title'>{van.name}</h2>
+                    <p className='van-price'>
+                        <span>
+                            {`$${van.price}`}
+                        </span>
+                        <span>
+                            /day
+                        </span>
+                    </p>
+                    <p className={`van-type ${van.type}`}>
+                        {van.type}
+                    </p>
+                </div>
+            ))}
+    </section>
+    </main>
+    <Footer/>
+    </>
   )
 }
 
