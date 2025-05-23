0af87383e17090f78b9c0341936df153192ece07
Firdavs Yuldashev <firdavsyuldashev111@gmail.com>
2025-05-23 12:39:52 +0500

    toastify message / email verification -  customization

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Vans from './pages/Vans/Vans';
